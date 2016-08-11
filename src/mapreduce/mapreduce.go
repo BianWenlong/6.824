70d8eaff2bb819773dd9183f38bc18464af4ddc4
bianwenlong <wenlong.bian@dianping.com>
2016-08-11 21:07:16 +0800

    add rpc mapreduce

--- src/mapreduce/mapreduce.go
+++ src/mapreduce/mapreduce.go
@@ -61,8 +61,8 @@ type MapReduce struct {
 	stats           *list.List
 
 	// Map of registered workers that you need to keep up to date
-	Workers map[string]*WorkerInfo
-
+	Workers        map[string]*WorkerInfo
+	UserWorkerList *list.List
 	// add any additional state here
 }
 
@@ -76,7 +76,8 @@ func InitMapReduce(nmap int, nreduce int,
 	mr.alive = true
 	mr.registerChannel = make(chan string)
 	mr.DoneChannel = make(chan bool)
-
+	mr.UserWorkerList = new(list.List)
+	mr.Workers = make(map[string]*WorkerInfo)
 	// initialize any additional state here
 	return mr
 }
--- src/mapreduce/master.go
+++ src/mapreduce/master.go
@@ -3,12 +3,10 @@ package mapreduce
 import "container/list"
 import "fmt"
 
-
 type WorkerInfo struct {
 	address string
 	// You can add definitions here.
 }
-
 
 // Clean up all workers by sending a Shutdown RPC to each one of them Collect
 // the number of jobs each work has performed.
@@ -30,5 +28,60 @@ func (mr *MapReduce) KillWorkers() *list.List {
 
 func (mr *MapReduce) RunMaster() *list.List {
 	// Your code here
+
+	go func() {
+		for {
+			select {
+			case newWorkerAddress := <-mr.registerChannel:
+				workInfo := WorkerInfo{newWorkerAddress}
+				mr.Workers[newWorkerAddress] = &workInfo
+				mr.UserWorkerList.PushFront(workInfo)
+				fmt.Println("worker register to master")
+			default:
+			}
+		}
+	}()
+
+	doMasterMap(mr, Map)
+	doMasterMap(mr, Reduce)
 	return mr.KillWorkers()
+}
+
+func doMasterMap(mr *MapReduce, jobType JobType) {
+	var nJob int
+	var otherJob int
+	chanMap := make(map[int]chan bool)
+	if jobType == Map {
+		nJob = mr.nMap
+		otherJob = mr.nReduce
+	} else {
+		nJob = mr.nReduce
+		otherJob = mr.nMap
+	}
+
+	for i := 0; i < nJob; {
+		if mr.UserWorkerList.Len() < 1 {
+			continue
+		} else {
+			workInfo := mr.UserWorkerList.Front().Value.(WorkerInfo)
+			jobArg := new(DoJobArgs)
+			jobArg.File = mr.file
+			jobArg.Operation = jobType
+			jobArg.JobNumber = i
+			jobArg.NumOtherPhase = otherJob
+			jobReply := new(DoJobReply)
+			done := make(chan bool)
+			go func() {
+				call(workInfo.address, "Worker.DoJob", jobArg, jobReply)
+				mr.UserWorkerList.PushFront(workInfo)
+				done <- true
+			}()
+			chanMap[i] = done
+			i++
+		}
+	}
+
+	for _, done := range chanMap {
+		<-done
+	}
 }
